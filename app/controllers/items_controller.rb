ff3906432fb85701746fb203ae653a9b0f0bed60
Keila Lopez <keilalofra@gmail.com>
2022-12-18 13:33:39 -0600

    first commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,16 @@ class ItemsController < ApplicationController
     render json: items, include: :user
   end
 
+  def show
+    # user = User.find_by(id: params[:id])
+    # render json: user, include: :items
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
+    render items
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,20 @@
 class UsersController < ApplicationController
 
+  def index
+    users = User.all
+    render json: users
+  end
+
   def show
     user = User.find_by(id: params[:id])
     render json: user, include: :items
+    # if params[:user_id]
+    #   user = User.find(params[:user_id])
+    #   items = user.items
+    # else
+    #   items = Item.all
+    # end
+    # render items
   end
 
 end
